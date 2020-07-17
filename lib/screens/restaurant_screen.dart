502331a9634c6d9b146f25686a7e7992f81c22c5
sanikumar sahani <sanikumarsahani@gmail.com>
2020-07-17 19:07:50 +0530

    :sparkles: resetaurant page

---
+++ lib/screens/restaurant_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_food_delivery_ui/models/restaurant.dart';
+
+class RestaurantScreen extends StatefulWidget {
+  final Restaurant restaurant;
+
+  RestaurantScreen({this.restaurant});
+  @override
+  _RestaurantScreenState createState() => _RestaurantScreenState();
+}
+
+class _RestaurantScreenState extends State<RestaurantScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              Image(
+                image: AssetImage(widget.restaurant.imageUrl),
+                height: 220.0,
+                width: MediaQuery.of(context).size.width,
+                fit: BoxFit.cover,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical:50.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                      icon: Icon(Icons.arrow_back),
+                      color: Colors.white,
+                      iconSize: 30.0,
+                      onPressed: () => Navigator.pop(context),
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.favorite),
+                      color: Colors.red,
+                      iconSize: 30.0,
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
