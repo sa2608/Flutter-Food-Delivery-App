72cfbb361a514699bcf1fd8334d547d2a200fe48
sanikumar sahani <sanikumarsahani@gmail.com>
2020-07-17 20:41:39 +0530

    :lipstick: restaurant reviews and contact button added

--- lib/screens/restaurant_screen.dart
+++ lib/screens/restaurant_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_food_delivery_ui/models/restaurant.dart';
+import 'package:flutter_food_delivery_ui/widgets/rating_stars.dart';
 
 class RestaurantScreen extends StatefulWidget {
   final Restaurant restaurant;
@@ -19,7 +20,7 @@ class _RestaurantScreenState extends State<RestaurantScreen> {
             children: [
               Hero(
                 tag: widget.restaurant.imageUrl,
-                              child: Image(
+                child: Image(
                   image: AssetImage(widget.restaurant.imageUrl),
                   height: 220.0,
                   width: MediaQuery.of(context).size.width,
@@ -27,7 +28,8 @@ class _RestaurantScreenState extends State<RestaurantScreen> {
                 ),
               ),
               Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical:50.0),
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 20.0, vertical: 50.0),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
@@ -45,7 +47,79 @@ class _RestaurantScreenState extends State<RestaurantScreen> {
                     ),
                   ],
                 ),
-              )
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      widget.restaurant.name,
+                      style: TextStyle(
+                        fontSize: 22.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      '0.2 miles away',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
+                ),
+                RatingStars(widget.restaurant.rating),
+                SizedBox(
+                  height: 6.0,
+                ),
+                Text(
+                  widget.restaurant.address,
+                  style: TextStyle(
+                    fontSize: 18.0,
+                  ),
+                )
+              ],
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              FlatButton(
+                padding: EdgeInsets.symmetric(horizontal: 30.0),
+                color: Theme.of(context).primaryColor,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Text(
+                  'Reviews',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                  ),
+                ),
+                onPressed: () {},
+              ),
+              FlatButton(
+                padding: EdgeInsets.symmetric(horizontal: 30.0),
+                color: Theme.of(context).primaryColor,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Text(
+                  'Contact',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                  ),
+                ),
+                onPressed: () {},
+              ),
             ],
           )
         ],
